8272a2f9833e249ed8d9bcbe3f9c3ba78bac6646
Gregory Feliu <36009037+gregfeliu@users.noreply.github.com>
2021-05-15 10:48:41 -0400

    Add files via upload

--- src/functions.py
+++ src/functions.py
@@ -3,12 +3,15 @@ import pandas as pd
 import pickle
 import geopandas as gpd
 from shapely.ops import nearest_points
-from shapely.geometry import LineString
+from shapely.geometry import LineString, Point
 import scipy.stats as st
 from clustergram import Clustergram
 import urbangrammar_graphics as ugg
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import scale, MinMaxScaler
+import numpy as np
 
 
 # IMPORTING DATA
@@ -26,7 +29,6 @@ def make_bare_api_call(additional_text):
 def find_player_ids(team_roster_json, team_id):
     assert type(team_roster_json) == dict
     assert type(team_id) == int
-#     player_list = [team_roster_json['roster'][x]['person'] for x in range(len(team_roster_json['roster']))]
     player_list = []
     for x in range(len(team_roster_json['roster'])):
         player_dict = team_roster_json['roster'][x]['person']
@@ -139,8 +141,8 @@ def get_best_distribution(data):
 
 ##### Optimal number of cluster #####
 # both of these were made using help from the clustergram page https://clustergram.readthedocs.io/en/latest/
-def fit_plot_clustergram(scaled_data):
-    cgram = Clustergram(range(1, 10), n_init=1000, verbose=False)
+def fit_plot_clustergram(scaled_data, end_range=10):
+    cgram = Clustergram(range(1, end_range), n_init=1000, verbose=False)
     cgram.fit(scaled_data)
     ax = cgram.plot(
     figsize=(10, 8),
@@ -151,7 +153,7 @@ def fit_plot_clustergram(scaled_data):
     sns.despine(offset=10)
     return cgram
 
-def evaluate_num_of_clusters(clustergram):  
+def visualize_num_of_clusters(clustergram):  
     fig, axs = plt.subplots(2, figsize=(10, 10), sharex=True)
     # score of 1 is best with the silhouette score
     clustergram.silhouette_score().plot(
@@ -164,10 +166,76 @@ def evaluate_num_of_clusters(clustergram):
         ax=axs[1]
     )
     sns.despine(offset=10)
+    
+def evaluate_num_of_clusters(clustergram):
+    # create silhouette_score 
+    min_max_scaler = MinMaxScaler()
+    silhouette_score_data = np.array(clustergram.silhouette_score()).reshape(-1, 1)
+    min_max_scaler.fit(silhouette_score_data)
+    scaled_sil_data = min_max_scaler.transform(silhouette_score_data)
+    # get calinski_harabasz_score data 
+    # min_max_scaler_c = MinMaxScaler()
+    calinski_score_data = np.array(clustergram.calinski_harabasz_score()).reshape(-1, 1)
+    min_max_scaler.fit(calinski_score_data)
+    scaled_cal_data = min_max_scaler.transform(calinski_score_data)
+    cluster_stat_combined = scaled_sil_data + scaled_cal_data
+    # index starts at 2
+    best_index = np.where(cluster_stat_combined == cluster_stat_combined.max())
+    # return the list of values produced and the best index
+    return cluster_stat_combined, best_index[0][0] + 2
+    
+def make_dbscan_cluster(esp_param, min_samples_param, data):
+    dbscan_model = DBSCAN(eps=esp_param, min_samples=min_samples_param)
+    dbscan_yhat = dbscan_model.fit_predict(data)
+    return dbscan_yhat
 
+def determine_if_noise(dbscan_predictions):
+    # determining if the player's birthplace was placed into a cluster, or was considered noise
+    noisey_data = np.where(dbscan_predictions==-1, 0, 1)
+    return noisey_data
+
+def run_cluster_determine_noise(esp_param, min_samples_param, data):
+    dbscan_predictions = make_dbscan_cluster(esp_param, min_samples_param, data)
+    noise_determination = determine_if_noise(dbscan_predictions)
+    return noise_determination
+    
 ##### Plotting the clusters #####
-def plot_clusters(us_can_gdf, clustered_gdf, col_name_to_plot: str):
-    
-    
-    
+def add_kmeans_centroids_to_gdf(clustered_gdf, fitted_kmeans):
+    num_clusters = len(fitted_kmeans.cluster_centers_)
+    if 'fullName' in clustered_gdf.columns:
+        centroid_gdf = gpd.GeoDataFrame(data=[f"Cluster{x}" for x in range(0, num_clusters)], columns = ['fullName'])
+    elif 'Team_name' in clustered_gdf.columns:
+        centroid_gdf = gpd.GeoDataFrame(data=[f"Cluster{x}" for x in range(0, num_clusters)], columns = ['Team_name'])
+    # making the centroids into points
+    centroid_gdf['geometry'] = [Point(coord[1], coord[0]) for coord in fitted_kmeans.cluster_centers_]
+    if 'player_cluster' in clustered_gdf.columns:
+        centroid_gdf['player_cluster'] = [x for x in range(num_clusters, (num_clusters*2))]
+    elif 'arena_cluster' in clustered_gdf.columns:
+        centroid_gdf['arena_cluster'] = [x for x in range(num_clusters, (num_clusters*2))]
+    clustered_gdf_w_centroids = pd.concat([clustered_gdf, centroid_gdf])
+    return clustered_gdf_w_centroids
+
+def plot_clusters(us_can_gdf, clustered_gdf, col_name_to_plot: str, include_legend=False):
+    ax = us_can_gdf.plot(figsize = (12, 12), facecolor='white', edgecolor='grey')
+    clustered_gdf.plot(column=col_name_to_plot, cmap='tab20b', ax=ax, figsize = (12, 12), legend=include_legend)
     return ax
+
+##### Finding City Name for Each Cluster #####
+def assign_clusters_names(dataframe, city_name_list: list):
+    cluster_to_name_dict = {str(x): None for x in dataframe['player_cluster_dbscan'].unique()}
+    counter = 0
+    while counter < len(cluster_to_name_dict):
+        for idx in range(len(dataframe)):
+            if dataframe['city_province'].iloc[idx] in city_name_list:
+                cluster_num = str(dataframe['player_cluster_dbscan'].iloc[idx])
+                if cluster_to_name_dict[cluster_num] == None:
+                    city_name = dataframe['city_province'].iloc[idx].split(',')[0]
+                    # changing Regina and Brooklyn and Montreal
+                    name_switch_dict = {"Regina":'Saskatchewan', 'Brooklyn': 'NYC', "Montreal": "Montreal/Ottawa"
+                                       }
+                    if city_name in name_switch_dict.keys():
+                        city_name = name_switch_dict[city_name]
+                    cluster_to_name_dict[cluster_num] = city_name
+                counter +=1 
+    dataframe['Cluster_name'] = dataframe['player_cluster_dbscan'].astype('str').replace(cluster_to_name_dict)
+    return dataframe
